bbd9307838da6f674db9823307195f9a06af400c
Nikasan1 <nikasan4@gmail.com>
2023-10-11 10:04:58 +0200

    Added new so println(2)

--- src/Homework.java
+++ src/Homework.java
@@ -2,5 +2,6 @@ public class Homework {
     public static void main(String[] args){
         System.out.println("Hello world and Git!");
         System.out.println("Hello world and Git, second commit.");
+        System.out.println("Hello Git again.");
     }
 }
